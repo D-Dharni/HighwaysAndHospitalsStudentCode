0b7f0030bbc7ccf2bb2251b1df451c4a9eb957a5
devendharni <deven.dharni@gmail.com>
2025-09-14 16:06:29 -0700

    - Did some setting up for the adjacency matrix - Filled the matrix in

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Deven Dharni
  *
  */
 
@@ -15,6 +18,45 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+
+        // If hospitals cost less than highways then (n) * hospital cost
+        if (hospitalCost < highwayCost) {
+            return n * hospitalCost;
+        }
+
+        // For each city have a list of its neighbors for easy access
+        List<List<Integer>> adjacencyGraph= new ArrayList<>();
+
+        // Loop through each city and add an empty list for each city
+        for (int i = 0; i < n+1; i++) {
+            ArrayList<Integer> arr = new ArrayList<Integer>();
+            adjacencyGraph.add(arr);
+        }
+
+        // Fill in the adjacencyGraph by using cities[][]
+
+        for (int i = 0; i < cities.length; i++) {
+            // Create variables for better fluency
+            int firstPair = cities[i][0];
+            int secondPair = cities[i][1];
+
+            // Update the first pair's to the second
+            List<Integer> current = adjacencyGraph.get(firstPair);
+            current.add(secondPair);
+
+            // Update the second pair's to the first
+            current = adjacencyGraph.get(secondPair);
+            current.add(firstPair);
+        }
+
+        // Find connected parts using BFS
+
+        // Compute the cost per cluster
+
+            // For each cluster do hospitalCost + (clusterSize - 1) * highwayCost
+
+        // Sum the total cost
+
         return 0;
     }
 }
