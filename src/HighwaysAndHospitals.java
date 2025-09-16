760b39e66fc98a9899077096fc027f752a1e51fa
devendharni <deven.dharni@gmail.com>
2025-09-15 19:17:24 -0700

    -Started to loop through each of the cities to find the clusters. -Wrote all the pseudocode for the BFS of finding the city size for each cluster.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -49,14 +49,50 @@ public class HighwaysAndHospitals {
             current.add(firstPair);
         }
 
-        // Find connected parts using BFS
+        // Create an array for visited cities
+        boolean[] visitedCities = new boolean[n+1];
 
-        // Compute the cost per cluster
+        // Set the cost variable
+        double totalCost = 0;
 
-            // For each cluster do hospitalCost + (clusterSize - 1) * highwayCost
-
-        // Sum the total cost
+        // Start to loop through the all the cities
+        for (int i = 1; i < n+1; i++) {
+            // First check if you haven't visited the item
+            if (visitedCities[i] = false) {
+                // Start BFS by calling the BFS function
+            }
+        }
 
         return 0;
+    }
+
+    public static int bfs(List<List<Integer>> adjacencyGraph, int start, boolean visited) {
+        // Make the queue
+
+        // Set the city count to 0
+
+        // Add the start city to the queue
+
+        // Set it's visited to true
+
+        // Increment the city counter
+
+        // While the queue isn't empty
+
+            // Poll the first city
+
+            // Loop through the neighbors of the adjacent cities
+
+                // Check if you haven't visited the neighbor
+
+                    // Set the neighbor's visited to true
+
+                    // Add the neighbor to the queue
+
+                    // Increment the city count
+
+        // Return the city count
+
+        return -1;
     }
 }
